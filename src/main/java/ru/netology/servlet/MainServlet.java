6a1c157c809e7a7c5e91a65495f45518bca04b4f
Arti <artmasterae@mail.ru>
2024-10-20 12:53:32 +0300

    Java Config+

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -16,7 +16,7 @@ public class MainServlet extends HttpServlet {
 @Override
 public void init() {
   final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
-  final var controller = context.getBean("postController");
+  controller = (PostController) context.getBean("postController");
   final var service = context.getBean(PostService.class);
 }
 
